c12726cce8c4a8f10493c7fa4d753a021f8e31ee
Ryan Mercadante <ryan.a.mercadante@gmail.com>
2021-09-25 13:30:28 -0400

    add function to get total waves

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -12,10 +12,11 @@ contract WavePortal {
 
     function wave() public {
         totalWaves += 1;
-        console.log(
-            "%s has waved and now totalWaves is %s",
-            msg.sender,
-            totalWaves
-        );
+        console.log("%s has waved!", msg.sender);
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("We have %d total waves!", totalWaves);
+        return totalWaves;
     }
 }
