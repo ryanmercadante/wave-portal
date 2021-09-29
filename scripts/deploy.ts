0c90e5a48997a843f12f31bd97a68c637d6ddc2a
Ryan Mercadante <ryan.a.mercadante@gmail.com>
2021-09-28 20:58:26 -0400

    add deploy script and nextjs

---
+++ scripts/deploy.ts
@@ -0,0 +1,26 @@
+import { ethers } from 'hardhat'
+
+const main = async () => {
+  const [deployer] = await ethers.getSigners()
+  const accountBalance = await deployer.getBalance()
+
+  console.log('Deploying contracts with account: ', deployer.address)
+  console.log('Account balance: ', accountBalance.toString())
+
+  const Token = await ethers.getContractFactory('WavePortal')
+  const portal = await Token.deploy()
+
+  console.log('WavePortal address: ', portal.address)
+}
+
+const runMain = async () => {
+  try {
+    await main()
+    process.exit(0)
+  } catch (error) {
+    console.error(error)
+    process.exit(1)
+  }
+}
+
+runMain()
